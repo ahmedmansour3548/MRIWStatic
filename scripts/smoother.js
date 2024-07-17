7b1061c383432a8379041a1232c4f5a5764b27e1
ahmedmansour3548 <120754004+ahmedmansour3548@users.noreply.github.com>
2024-07-17 12:17:18 -0400

    Update smoother.js

--- scripts/smoother.js
+++ scripts/smoother.js
@@ -8,13 +8,12 @@ AFRAME.registerComponent("smoother", {
      if (this.el.object3D.visible) {
        this.target.setAttribute('visible', 'true')
        if(this.prevPosition) {
-        console.log("lerping", this.el.object3D.position);
          this.target.object3D.position.lerp(this.prevPosition, 0.1)
          let rot = this.target.object3D.rotation.toVector3().lerp(this.prevRotation, 0.1)
-         this.target.object3D.rotation.setFromVector3(rot)
+         //this.target.object3D.rotation.setFromVector3(rot)
        } else {
          this.target.setAttribute('position', this.el.getAttribute('position'))
-         //this.target.setAttribute('rotation', this.el.getAttribute('rotation'))
+         this.target.setAttribute('rotation', this.el.getAttribute('rotation'))
        }
        this.prevPosition = this.el.object3D.position
        this.prevRotation = this.el.object3D.rotation
