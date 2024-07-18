6724c006d4ea2e3ab8dd89a28bf13d4ab95b33e2
ahmedmansour3548 <120754004+ahmedmansour3548@users.noreply.github.com>
2024-07-18 12:32:22 -0400

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -69,7 +69,7 @@ AFRAME.registerComponent('click-listener', {
         });
 
         // Enable fog in the scene
-        this.el.sceneEl.object3D.fog = new THREE.Fog(0x000000, 15, 20); // Fog color and density
+        this.el.sceneEl.object3D.fog = new THREE.Fog(0x000000, 18, 22); // Fog color and density
         // Tracking Variables
         this.doorsOpen = false; // Current state of doors
         this.modelURL = null; // Path to model url (.gltf or .glb)
